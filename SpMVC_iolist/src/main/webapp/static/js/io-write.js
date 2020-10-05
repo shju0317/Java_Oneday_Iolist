cd83d04026894a4e49840e2984a8c5bdaa62d0b1
shju0317 <shju0317@naver.com>
2020-10-05 16:40:17 +0900

    modify

--- SpMVC_iolist/src/main/webapp/static/js/io-write.js
+++ SpMVC_iolist/src/main/webapp/static/js/io-write.js
@@ -6,11 +6,7 @@ $(function () {
   });
 
   $(document).on("click", "div.book-select", function () {
-    $.ajax({
-      url: `${rootPath}/api/isbn`,
-      method: "POST",
-      data: { search_text: isbn },
-    })
+    $.ajax()
       .done(function (productVO) {
         // alert(JSON.stringify(productVO))
 
@@ -22,11 +18,13 @@ $(function () {
         $("#io_price").val(productVO.io_price);
         $("#io_quan").val(productVO.io_quan);
         
-        let price = $(this).data("io_price");
-        let quan = $(this).data("io_quan");
+        let price = $("input").data("io_price");
+        let quan = $("input").data("io_quan");
         let total = price * quan;
         
         $("#io_total").val(total);
+        alert(total);
+        
       })
       .fail(function (xhr, textStatus, error) {
         alert("서버와 통신오류 :()");
